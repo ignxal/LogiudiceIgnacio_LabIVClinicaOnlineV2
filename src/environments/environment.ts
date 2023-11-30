b63402373123a2c72aec42eda10917db81cd06e0
ignxal <ignacio.logiudice@gmail.com>
2023-11-29 22:54:38 -0300

    feat: add env + index

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,11 @@
 export const environment = {
   firebaseConfig: {
+    projectId: 'clinica-online-v2',
+    appId: '1:609728948069:web:9d10121c99fd640a2c238c',
+    storageBucket: 'clinica-online-v2.appspot.com',
+    locationId: 'us-central',
     apiKey: 'AIzaSyArnK_dkzKgiGkKBMkJYbNelcdE6OVVsFA',
     authDomain: 'clinica-online-v2.firebaseapp.com',
-    projectId: 'clinica-online-v2',
-    storageBucket: 'clinica-online-v2.appspot.com',
     messagingSenderId: '609728948069',
-    appId: '1:609728948069:web:9d10121c99fd640a2c238c',
   },
 };
