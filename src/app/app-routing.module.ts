9a1363eae21efd1126df14749824a039baa7f99c
ignxal <ignacio.logiudice@gmail.com>
2023-11-30 00:37:25 -0300

    feat: update app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { RolesGuard } from './guards/role.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./components/login/login.module').then((m) => m.LoginModule),
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./components/admin/admin.module').then((m) => m.AdminModule),
+    canActivate: [RolesGuard],
+    data: { role: ['Admin'], redirect: '/' },
+  },
+  {
+    path: 'register',
+    loadChildren: () =>
+      import('./components/register/register.module').then(
+        (m) => m.RegisterModule
+      ),
+  },
+  {
+    path: 'mailVerified',
+    loadChildren: () =>
+      import('./components/verification/verification.module').then(
+        (m) => m.VerificationModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SharedModule } from './components/shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { environment } from 'src/environments/environment';
 import { getApp, initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideStorage, getStorage } from '@angular/fire/storage';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { environment } from 'src/environments/environment';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, WelcomeComponent],
   imports: [
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth(getApp())),
@@ -17,6 +24,12 @@ import { environment } from 'src/environments/environment';
     provideStorage(() => getStorage(getApp())),
     BrowserModule,
     AppRoutingModule,
+    SharedModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatListModule,
+    MatGridListModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
